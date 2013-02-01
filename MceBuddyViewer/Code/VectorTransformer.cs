ed17e813c4b7170bdbf1a6c6cffbd309fe122466
andrei_kr_cp <andreikr79@users.noreply.github.com>
2013-02-01 12:53:11 +0000

    Add ProgressBar UI

---
+++ MceBuddyViewer/Code/VectorTransformer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.MediaCenter.UI;
+
+namespace MceBuddyViewer
+{
+    /// <summary>
+    /// Transformer for converting one or more fractional values into a Vector3 representation.
+    /// </summary>
+    public class VectorTransformer : ITransformer
+    {
+
+        /// <summary>
+        /// Format string that dictates which dimension of the vector is
+        /// represented by the value, e.g. "{0},1,1". The remaining dimensions
+        /// are taken from the format string itself.
+        /// </summary>
+        public string Format { get; set; }
+
+        /// <summary>
+        /// Transforms the input value into a Vector3 representation.
+        /// </summary>
+        /// <param name="value">Input value.</param>
+        /// <returns>Output value.</returns>
+        public object Transform(object value)
+        {
+            string[] parts = String.Format(Format, value).Split(',');
+            return new Vector3(
+                Single.Parse(parts[0]),
+                Single.Parse(parts[1]),
+                Single.Parse(parts[2])
+            );
+        }
+    }
+}
